e68f1ba604181c44bdee67b186f8e3901a4a2b67
lippo97 <filo_97@hotmail.it>
2016-08-28 23:26:56 +0200

    Funziona il login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,18 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      #
-      #
+      log_in user
       redirect_to forwarding_url
     else
       flash[:trigger_modal] = "login"
       flash[:login_error] = 'Combinazione email/password non valida'
       redirect_to forwarding_url || root_path
     end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
   end
 
   private
---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "login with invalid information" do
+    get about_path
+    assert_template 'static_pages/about'
+    post_via_redirect login_path, session: {email: "", password: "", forwarding_url: about_path}
+    assert_template 'static_pages/about'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+    assert_not is_logged_in?
+  end
+
+  test "login with valid information then logout" do
+    get about_path
+    assert_template 'static_pages/about'
+    post_via_redirect login_path, session: {email: @user.email, password: 'password', forwarding_url: about_path}
+    assert_template 'static_pages/about'
+    assert flash.empty?
+    assert_select "a[href=?]", user_path(@user)
+    assert is_logged_in?
+    delete_via_redirect logout_path
+    assert_template 'static_pages/home'
+    assert_not is_logged_in?
+  end
+end
