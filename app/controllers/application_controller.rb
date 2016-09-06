dafa21c33a1d4415ed2625248b03ecd73bd75a53
a-marfoglia <a.marfoglia@hotmail.it>
2016-09-06 16:49:32 +0200

    Aggiunte tante cose, concluso niente

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+  
+  private
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:trigger_modal] = 'login'
+        flash[:login_error] = "Area riservata, effettua prima il login"
+        redirect_to root_path
+      end
+    end
 end
--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,30 @@
 class MicropostsController < ApplicationController
+before_action :logged_in_user, only: [:new, :create]
+
   def index
     @microposts = Micropost.all  
   end
+  
+  def new
+    @micropost = Micropost.new
+  end
+  
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      redirect_to @micropost
+    else
+      flash.now[:danger] = @micropost.errors.full_messages
+      render 'new'
+    end
+  end
+  
+  def show
+    @micropost = Micropost.find(params[:id])
+  end
+  
+  private
+    def micropost_params
+      params.require(:micropost).permit(:title, :content)
+    end
 end
