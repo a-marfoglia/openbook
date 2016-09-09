2bf430b2890a2cc32040cbeaa49fada72a67d92c
lippo97 <filo_97@hotmail.it>
2016-09-09 18:10:36 +0200

    Modified comments integration test

--- test/integration/comments_test.rb
+++ test/integration/comments_test.rb
@@ -10,7 +10,9 @@ class CommentsTest < ActionDispatch::IntegrationTest
   test 'should redirect if not logged in, else post' do
     get micropost_path(@micropost)
     assert_template 'microposts/show'
-    post_via_redirect comments_path comment: { content: "Example", micropost_id: @micropost.id }
+    assert_no_difference 'Comment.count' do
+      post_via_redirect comments_path comment: { content: "Example", micropost_id: @micropost.id }    
+    end
     assert_template 'static_pages/home'
     assert_select 'div[class=?]', 'alert alert-danger', count: 1
     log_in_as(@user)
